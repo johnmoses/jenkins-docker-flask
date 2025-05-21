63c5a0ccbdc4d8e76eee39702db0e4b1f8af0bad
johnmoses <johnmosesng@gmail.com>
2025-05-21 15:14:02 +0100

    updated 2

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Welcome to My Flask API"})
+    return jsonify({"message": "Welcome to My Flask API 1"})
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
